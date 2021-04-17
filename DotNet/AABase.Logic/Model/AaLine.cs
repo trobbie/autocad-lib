8af15476aa5e9367ab5bfd1e1687a5fd9d09d1e0
Trevor Robbie <trobbie@gmail.com>
2021-04-17 15:25:55 -0400

    Added IEquatable and IComparable

--- DotNet/AABase.Logic/Model/AaLine.cs
+++ DotNet/AABase.Logic/Model/AaLine.cs
@@ -1,7 +1,9 @@
 
+using System;
+
 namespace AABase.Logic
 {
-    public class AaLine
+    public class AaLine : IEquatable<AaLine>, IComparable<AaLine>
     {
         private readonly AaPoint3d _pt1;
         private readonly AaPoint3d _pt2;
@@ -12,15 +14,39 @@ namespace AABase.Logic
           _pt2 = pt2;
         }
 
-        public bool Equals(AaLine line)
-        {
-            // check both points, either order
-            return (_pt1.Equals(line._pt1) && _pt2.Equals(line._pt2))
-                || (_pt1.Equals(line._pt2) && _pt2.Equals(line._pt1));
-        }
+        public override bool Equals(object obj) => Equals(obj as AaLine);
         public override int GetHashCode()
         {
             return _pt1.GetHashCode() ^ _pt2.GetHashCode();
         }
+
+        public bool Equals(AaLine line)
+        {
+            // check both points, either order
+            return line != null 
+                && _pt1.Equals(line._pt1)
+                && _pt2.Equals(line._pt2);
+        }
+        public bool EqualsIgnorePointOrder(AaLine line)
+        {
+            // check both points, either order
+            return line != null 
+                && ((_pt1.Equals(line._pt1) && _pt2.Equals(line._pt2))
+                    || (_pt1.Equals(line._pt2) && _pt2.Equals(line._pt1)));
+        }
+        public int CompareTo(AaLine other)
+        {
+            // If other is not a valid object reference, this instance is greater.
+            if (other == null) return 1;
+
+            int result = _pt1.X.CompareTo(_pt2.X);
+            return (result != 0) ? result : _pt1.Y.CompareTo(_pt2.Y);
+        }
+        
+        public override string ToString()
+        {
+        return $"[{_pt1.ToString()},{_pt2.ToString()}]";
+        }
+
     }
 }
