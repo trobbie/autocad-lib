866eb346fa1a61ca658e2f5ad7d565a215f5e83b
Trevor Robbie <trobbie@gmail.com>
2021-04-18 09:40:36 -0400

    Added support to filter passed entities

--- DotNet/AABase.Logic/Extension/AaLineExtensions.cs
+++ DotNet/AABase.Logic/Extension/AaLineExtensions.cs
@@ -6,11 +6,18 @@ namespace AABase.Logic
 {
     public static class AaLineExtensions
     {
+        private static IEnumerable<string> _supportedDxfNamesForGetLineList = new List<string> { "LINE", "LWPOLYLINE" };
+
+        public static bool IsDxfNameSupportedForGetLineList(string dxfName) 
+        {
+            return _supportedDxfNamesForGetLineList.Contains(dxfName);
+        }
         public static string ToStringDebug(this IEnumerable<AaLine> listLines)
         {
             return listLines.Select(i => i.ToString())
                             .Aggregate("",(i,j) => i + "," + j);
         }
+
         public static List<AaLine> GetLineList(this IEnumerable<IEntity> listLines)
         {
             List<AaLine> result = new List<AaLine>();
@@ -49,10 +56,13 @@ namespace AABase.Logic
                         }
                         break;
                     case "CIRCLE":
+                        // TODO: support arcs; add a CircularArc3d base type
                         throw new NotImplementedException();
                     case "ARC":
+                        // TODO: support arcs; add a CircularArc3d base type
                         throw new NotImplementedException();
                     case "BLOCK":
+                        // TODO: support blocks
                         throw new NotImplementedException();
                     default:
                         throw new NotImplementedException();
