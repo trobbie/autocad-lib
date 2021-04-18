aca3dbaf1882455f9a3a7c2ca048903612ee68d3
Trevor Robbie <trobbie@gmail.com>
2021-04-18 08:57:42 -0400

    Added more tests

--- DotNet/AABase.Tests/Extensions/AaLineExtensionsTests.cs
+++ DotNet/AABase.Tests/Extensions/AaLineExtensionsTests.cs
@@ -10,12 +10,18 @@ namespace AABase.Tests
     {
         private static readonly List<FakeEntity> emptyList = new List<FakeEntity>();
         private static readonly List<FakeEntity> triangleLines1 = new List<FakeEntity>();
+        private static readonly List<FakeEntity> rectangleClosedPolyline1 = new List<FakeEntity>();
+        private static readonly List<FakeEntity> rectangleOpenPolyline1 = new List<FakeEntity>();
+        
        [ClassInitialize]
         public static void InitializeFakeData(TestContext testContext) {
             // triangle (as individual lines)
             triangleLines1.Add(new FakeLineEntity(0, 0, 10, 0));
             triangleLines1.Add(new FakeLineEntity(10, 0, 10, 10));
             triangleLines1.Add(new FakeLineEntity(10, 10, 0, 0));
+
+            rectangleClosedPolyline1.Add(new FakeRectangle(new AaPoint3d(5,5), 7, 8, false));
+            rectangleOpenPolyline1.Add(new FakeRectangle(new AaPoint3d(5,5), 7, 8, true));
         }
         
         static void TestGetLineList(IEnumerable<IEntity>listObjects, List<AaLine> expected)
@@ -36,7 +42,7 @@ namespace AABase.Tests
         }
 
         [TestMethod]
-        public void GetLineList_TEST1()
+        public void GetLineList_MultipleLines_ReturnValid()
         { 
             List<AaLine> listLinesExpected = new List<AaLine>();
             listLinesExpected.Add(new AaLine(new AaPoint3d(0,0), new AaPoint3d(10,0)));
@@ -44,6 +50,30 @@ namespace AABase.Tests
             listLinesExpected.Add(new AaLine(new AaPoint3d(10,10), new AaPoint3d(0,0)));
 
             TestGetLineList(triangleLines1, listLinesExpected);
+        }
+
+        [TestMethod]
+        public void GetLineList_OneClosedPolyline_ReturnValid()
+        {
+            List<AaLine> listLinesExpected = new List<AaLine>();
+            listLinesExpected.Add(new AaLine(new AaPoint3d(5,5), new AaPoint3d(12,5)));
+            listLinesExpected.Add(new AaLine(new AaPoint3d(12,5), new AaPoint3d(12,13)));
+            listLinesExpected.Add(new AaLine(new AaPoint3d(12,13), new AaPoint3d(5,13)));
+            listLinesExpected.Add(new AaLine(new AaPoint3d(5,13), new AaPoint3d(5,5)));
+
+            TestGetLineList(rectangleClosedPolyline1, listLinesExpected);
+        }
+
+        [TestMethod]
+        public void GetLineList_OneOpenPolyline_ReturnValid()
+        {
+            List<AaLine> listLinesExpected = new List<AaLine>();
+            listLinesExpected.Add(new AaLine(new AaPoint3d(5,5), new AaPoint3d(12,5)));
+            listLinesExpected.Add(new AaLine(new AaPoint3d(12,5), new AaPoint3d(12,13)));
+            listLinesExpected.Add(new AaLine(new AaPoint3d(12,13), new AaPoint3d(5,13)));
+            listLinesExpected.Add(new AaLine(new AaPoint3d(5,13), new AaPoint3d(5,5)));
+
+            TestGetLineList(rectangleOpenPolyline1, listLinesExpected);
         }
 
     }
