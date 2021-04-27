0e0ec092010299dc53ae91c5fbe4f1cf653e9eab
Trevor Robbie <trobbie@gmail.com>
2021-04-27 07:31:40 -0400

    Added IArc methods

--- DotNet/AABase.Logic/AaInterface/IArc.cs
+++ DotNet/AABase.Logic/AaInterface/IArc.cs
@@ -5,5 +5,9 @@ namespace AABase.Logic
     {
         AaPoint3d Center { get; }
         double Radius { get; }
+        double StartAngle { get; }
+        double EndAngle { get; }
+        // the normal vector to the plane of the arc, expressed as simple 3d coordinate
+        AaPoint3d PlaneNormal { get; }
     }
 }
--- DotNet/AABase/AaWrapper/AaArc.cs
+++ DotNet/AABase/AaWrapper/AaArc.cs
@@ -11,6 +11,9 @@ namespace AABase.Logic
         private Arc GetArc() { return (Arc)_dbobject; }
         public AaPoint3d Center { get { return GetArc().Center.GetAaPoint(); } }
         public double Radius { get { return GetArc().Radius; } }
+        public double StartAngle { get { return GetArc().StartAngle; } }
+        public double EndAngle { get { return GetArc().EndAngle; } }
+        public AaPoint3d PlaneNormal { get { return new AaPoint3d(GetArc().Normal.ToArray()); } }
 
     }
 
