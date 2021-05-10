6dacb6e49c46493540823b8e93126e2f0232983a
Trevor Robbie <trobbie@gmail.com>
2021-05-10 08:37:47 -0400

    Added SameParentCurvesEqualityComparer

--- DotNet/AABase.Logic/Model/AaGeCurveOverlapResult.cs
+++ DotNet/AABase.Logic/Model/AaGeCurveOverlapResult.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using OverlapResultSummary = AABase.Logic.AaGeCurve.OverlapResultSummary;
 
 namespace AABase.Logic
@@ -9,6 +11,8 @@ namespace AABase.Logic
         public OverlapResultSummary Summary { get; set; }
         public AaGeCurve OverlapRegion { get; set; }
 
+        public static IEqualityComparer<AaGeCurveOverlapResult> SameParentCurvesEqualityComparer { get; } = new _SameParentCurvesEqualityComparer();
+        // Assumes thisCurve and otherCurve cannot be null
         public AaGeCurveOverlapResult(AaGeCurve thisCurve, AaGeCurve otherCurve)
         {
             ThisCurve =  thisCurve;
@@ -22,5 +26,25 @@ namespace AABase.Logic
             return this;
         } 
         
+        private class _SameParentCurvesEqualityComparer : IEqualityComparer<AaGeCurveOverlapResult>
+        {
+            public bool Equals(AaGeCurveOverlapResult x, AaGeCurveOverlapResult y)
+            {
+                if (Object.ReferenceEquals(x, y)) return true;
+                if (Object.ReferenceEquals(x, null) || Object.ReferenceEquals(y, null)) return false;
+
+                // Note: if AaGeCurveOverlapResult is instantiated, then ThisCurve and OtherCurve assumed never null
+                return ((x.ThisCurve == y.ThisCurve) && (x.OtherCurve == y.OtherCurve)
+                        ||  (x.ThisCurve == y.OtherCurve) && (x.OtherCurve == y.ThisCurve));
+
+            }
+            public int GetHashCode(AaGeCurveOverlapResult x)
+            {
+                if (Object.ReferenceEquals(x, null)) return 0;
+
+               // Note: if AaGeCurveOverlapResult is instantiated, then ThisCurve and OtherCurve assumed never null
+                return (x.ThisCurve.GetHashCode() ^ x.OtherCurve.GetHashCode());
+            }
+        }
     }
 }
