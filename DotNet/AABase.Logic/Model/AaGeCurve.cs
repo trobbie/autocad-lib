39a56c766bce90238b5277e4463094c0c4c176fb
Trevor Robbie <trobbie@gmail.com>
2021-05-29 20:50:14 -0400

    Moved ConvertEntitiesToCurveList

--- DotNet/AABase.Logic/Model/AaGeCurve.cs
+++ DotNet/AABase.Logic/Model/AaGeCurve.cs
@@ -382,48 +382,6 @@ namespace AABase.Logic
             return _supportedDxfNamesForCurveConversions.Contains(dxfName);
         }
 
-        public static List<AaGeCurve> ConvertEntitiesToCurveList(IEnumerable<IEntity> listCurves)
-        {
-            List<AaGeCurve> result = new List<AaGeCurve>();
-
-            // for each supported entity, add to the list of curves
-            foreach (IEntity entity in listCurves)
-            {
-                switch (entity.GetDxfName())
-                {
-                    case "LINE":
-                        ICurve line = (ICurve)entity;
-                        result.Add(new AaGeCurve(line.StartPoint, line.EndPoint));
-                        break;
-                    case "LWPOLYLINE": // light-weight polyline
-                        IPolyline pl = (IPolyline)entity;
-                        int numVertices = pl.NumberOfVertices;
-                        for (int i = 0; i < numVertices; i++)
-                        {
-                            // if on last vertex and closed, we're done with adding curves
-                            if (((i+1) == numVertices) && (!pl.Closed)) continue;
-                            result.Add(pl.GetGeCurveAt(i));
-                        }
-                        break;
-                    case "CIRCLE":
-                        ICircle circle = (ICircle)entity;
-                        result.Add(new AaGeCurve(circle.Center, circle.Radius, 0, 2*Math.PI, circle.PlaneNormal));
-                        break;
-                    case "ARC":
-                        IArc arc = (IArc)entity;
-                        result.Add(new AaGeCurve(arc.Center, arc.Radius, arc.StartAngle, arc.EndAngle, arc.PlaneNormal));
-                        break;
-                    case "BLOCK":
-                        // TODO: support blocks
-                        throw new NotImplementedException();
-                    default:
-                        throw new NotImplementedException();
-                }
-            }
-
-            return result;
-        }
-
         public override string ToString()
         {
             return IsArc
