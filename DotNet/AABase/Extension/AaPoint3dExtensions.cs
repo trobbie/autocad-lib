6b4b67b0edf3c8a4a176f9e8c7bd36f5b8e4ea3a
Trevor Robbie <trobbie@gmail.com>
2021-06-02 07:33:33 -0400

    Added GetAcPoint2d

--- DotNet/AABase/Extension/AaPoint3dExtensions.cs
+++ DotNet/AABase/Extension/AaPoint3dExtensions.cs
@@ -6,14 +6,22 @@ namespace AABase.Logic
 {
     public static class AaPoint3dExtensions
     {
-        public static Point3d GetAcPoint3d(this AaPoint3d ptAutocad)
+        public static Point3d GetAcPoint3d(this AaPoint3d aaPoint3d)
         {
-            return new Point3d(ptAutocad.AsArray());
+            return new Point3d(aaPoint3d.AsArray());
         }
 
         public static IEnumerable<Point3d> GetAcPoints(this AaPoint3d[] ptArrayAutocad)
         {
             return ptArrayAutocad.Select(pt => pt.GetAcPoint3d());
+        }
+
+        /// <summary>
+        /// Get point as Autocad Point2d, ignoring z-value
+        /// </summary>
+        public static Point2d GetAcPoint2d(this AaPoint3d aaPoint3d)
+        {
+            return new Point2d(aaPoint3d.AsArray());
         }
     }
 }
