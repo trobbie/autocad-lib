1f041f4474497a2ea63c30d5f6c2636cd00d8ee7
Trevor Robbie <trobbie@gmail.com>
2021-06-05 07:54:16 -0400

    Used UTF8 encoding

--- DotNet/AABase.Logic/Logging/AaTextWriter.cs
+++ DotNet/AABase.Logic/Logging/AaTextWriter.cs
@@ -6,6 +6,6 @@ namespace AABase.Logic.Logging
     public abstract class AaTextWriter : TextWriter
     {
         public override abstract void Write(char value);
-        public override Encoding Encoding { get { return Encoding.Default; }}
+        public override Encoding Encoding { get { return Encoding.UTF8; }}
     }
 }
